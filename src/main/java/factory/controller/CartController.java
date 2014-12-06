e49e5771670efaccb9f8107c3cb516311d82192a
AnnaBaumgaertel <Anna.Baumgaertel@mailbox.tu-dresden.de>
2014-12-06 14:54:36 +0100

    versuch einzelne sachen im warenkorb zu löschen

--- src/main/java/factory/controller/CartController.java
+++ src/main/java/factory/controller/CartController.java
@@ -4,8 +4,8 @@ import org.salespointframework.order.Cart;
 import org.salespointframework.quantity.Quantity;
 import org.salespointframework.quantity.Units;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -29,16 +29,24 @@ public class CartController {
 	}
     	
 	 @RequestMapping(value = "/warenkorb", method = RequestMethod.POST)
-	    public String addArticle(@RequestParam("pid") Article article, @RequestParam("number") int number, @ModelAttribute Cart cart,
-				ModelMap modelMap) {
+	    public String addArticle(@RequestParam("pid") Article article, @RequestParam("number") int number, @ModelAttribute Cart cart) {
 	    	
 			Quantity quantity = Units.of(number);
 			cart.addOrUpdateItem(article, quantity);
 			
 			return "redirect:/sortiment";
 	    }
+	 
+	 @RequestMapping(value = "/article/{id}", method = RequestMethod.POST)
+	    public String deleteArticle(@PathVariable Long pid, @RequestParam("pid") Article article, @ModelAttribute Cart cart) {
+	    	
+		 article.delete(pid);
+			
+			return "redirect:/warenkorb";
+	    }
+
+	 
 	    
-	
 	    @RequestMapping(value="/kaufen", method=RequestMethod.POST)   
 	    public String Buy(){
 	    	
