aa431de4c7215637238a7786547b714aa760c1cf
AnnaBaumgaertel <Anna.Baumgaertel@mailbox.tu-dresden.de>
2014-12-06 12:09:23 +0100

    Trennung von Shop und Cartcontroller

---
+++ src/main/java/factory/controller/CartController.java
@@ -0,0 +1,55 @@
+package factory.controller;
+
+import org.salespointframework.order.Cart;
+import org.salespointframework.quantity.Quantity;
+import org.salespointframework.quantity.Units;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import factory.model.Article;
+
+public class CartController {
+
+	
+	
+	 @RequestMapping(value = "/warenkorb", method = RequestMethod.POST)
+	    public String addArticle(@RequestParam("pid") Article article, @RequestParam("number") int number, @ModelAttribute Cart cart,
+				ModelMap modelMap) {
+	    	
+			Quantity quantity = Units.of(number);
+			cart.addOrUpdateItem(article, quantity);
+			
+			return "redirect:/sortiment";
+	    }
+	    
+	    
+		@ModelAttribute("cart")
+		private Cart getCart() {
+			return new Cart();
+		}
+	    
+		@RequestMapping(value = "/warenkorb", method = RequestMethod.GET)
+		public String basket() {
+			return "warenkorb";
+		}
+	    
+	
+	    @RequestMapping(value="/kaufen", method=RequestMethod.POST)   
+	    public String Buy(){
+	    	
+	    	return "kaufen";
+	}
+	    
+	    @RequestMapping(value = "cart/clear", method = RequestMethod.POST)
+		public String clear(@ModelAttribute Cart cart) {
+	    	
+	    	cart.clear();
+	    	
+			return "redirect:/sortiment";
+			
+		}   
+}
+
