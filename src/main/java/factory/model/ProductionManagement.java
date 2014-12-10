26051bf8c5b520c9b068a9bad68e693a5f1ff6cf
anna123456 <anna01210@mail.ru>
2014-12-10 22:56:53 +0100

    DateTest geschrieben

--- src/main/java/factory/model/ProductionManagement.java
+++ src/main/java/factory/model/ProductionManagement.java
@@ -1,35 +1,35 @@
-//package factory.model;
-//
-//import java.util.Calendar;
-//import java.util.Date;
-//import java.util.concurrent.TimeUnit;
-//
-//import org.springframework.stereotype.Component;
-//
+
+package factory.model;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
+
+
 //@Component
-//public class ProductionManagement {
-//
+public class ProductionManagement {
+
 //	private static final int DAY_IN_MILLIS = 24 * 3600 * 1000;
 //	private static final int VOLUME_HEKTOLITERS_IN_TWO_DAYS = 24;
 	
 /*
  * Datum wäre gut ;)
  */
-//	public long daysTillAprilTheFirst(Date date){
-//		Calendar cal = Calendar.getInstance();
-//		Calendar delDate = Calendar.getInstance();
-//		delDate.setTime(date);
-////		int curYear = cal.get(Calendar.YEAR); // Das aktuelle Jahr, oder das Jahr von date?
-//		int curYear = delDate.get(Calendar.YEAR); // Das aktuelle Jahr, oder das Jahr von date?
-//		int curMonth = cal.get(Calendar.MONTH);
-//		cal.set(curYear + (curMonth >= Calendar.APRIL ? 1:0), Calendar.APRIL, 1, 0, 0, 0);
-//		long thenMillis = cal.getTimeInMillis();
-//		long daysBetweenDates = (thenMillis - delDate.getTimeInMillis()) ;
-//		return TimeUnit.DAYS.convert(daysBetweenDates, TimeUnit.MILLISECONDS);
+	public long daysTillApril(Date date){
+		Calendar cal = Calendar.getInstance();
+		Calendar delDate = Calendar.getInstance();
+		delDate.setTime(date);
+		//		int curYear = cal.get(Calendar.YEAR); // Das aktuelle Jahr, oder das Jahr von date?
+		int curYear = delDate.get(Calendar.YEAR); // Das aktuelle Jahr, oder das Jahr von date?
+		int curMonth = cal.get(Calendar.MONTH);
+		cal.set(curYear + (curMonth >= Calendar.APRIL ? 1:0), Calendar.APRIL, 1, 0, 0, 0);
+		long thenMillis = cal.getTimeInMillis();
+		long daysBetweenDates = (thenMillis - delDate.getTimeInMillis()) ;
+		return TimeUnit.DAYS.convert(daysBetweenDates, TimeUnit.MILLISECONDS);
 ////		return daysBetweenDates;	
-//	}
-//
-//	public int countCapacityInHektoLiter (Date date){
+	}
+}//	public int countCapacityInHektoLiter (Date date){
 //		long daysTillAprilTheFirst = daysTillAprilTheFirst(date);
 //		long s = daysTillAprilTheFirst / 2 * VOLUME_HEKTOLITERS_IN_TWO_DAYS;
 //		return (int)s;
---
+++ src/test/java/videoshop/model/DateTest.java
@@ -0,0 +1,35 @@
+package videoshop.model;
+
+
+import static org.junit.Assert.assertEquals;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.junit.Test;
+
+
+import factory.model.ProductionManagement;
+
+
+public class DateTest {
+	@Test
+	public void testDate() throws ParseException{
+		DateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
+
+		Date date = formatter.parse("2014-12-10");
+		
+		ProductionManagement loc = new ProductionManagement ();
+		
+		int expected = 111;
+		int result = (int) loc.daysTillApril(date);
+		assertEquals(expected, result, 0);
+		System.out.println(expected + "=" + result);
+		
+		
+		}
+
+}
+
