e58485c4e8b6cd7eee28bfdb9f1d883402f9ab02
Daniel <daniel.fischer.dd@gmx.de>
2014-12-17 19:17:11 +0100

    :+1:

--- src/main/java/factory/model/Department.java
+++ src/main/java/factory/model/Department.java
@@ -11,38 +11,10 @@ public class Department {
 	@GeneratedValue
 	private Long id;
 	private String name;
-	//private double quantity; //Wert gibt an, wie viele Einheiten sich im Lager befinden
-	//private double capacity; //Wert gibt an, wie viel Produktionskapazität noch vorhanden ist
 
 	@Deprecated
 	public Department(){}
 
-	public Department(String name, double capacity) {
-		this.name = name;
-//		this.capacity = capacity;
-	}
-	
-//	public Department(String name, double quantity, double capacity) {
-//		this.name = name;
-//		this.quantity = quantity;
-////		this.capacity = capacity;
-//	}
-//	public double getQuantity() {
-//		return quantity;
-//	}
-//
-//	public void setQuantity(double quantity) {
-//		this.quantity = quantity;
-//	}
-//	
-//	public double getCapacity() {
-//		return capacity;
-//	}
-//
-//	public void setCapacity(double capacity) {
-//		this.capacity = capacity;
-//	}
-	
 	public Department(String name) {
 		this.name = name;
 	}
