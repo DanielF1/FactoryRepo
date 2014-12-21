3be877a525eebbaca68daf7b9e7fa169a1c16bde
Daniel <daniel.fischer.dd@gmx.de>
2014-12-21 17:12:40 +0100

    fehlerteufel

--- src/main/java/factory/model/EmployeeRepository.java
+++ src/main/java/factory/model/EmployeeRepository.java
@@ -1,6 +1,9 @@
 package factory.model;
 
+import org.salespointframework.useraccount.UserAccount;
 import org.springframework.data.repository.CrudRepository;
 
 public interface EmployeeRepository extends CrudRepository<Employee, Long> {
+
+	Employee findByUserAccount(UserAccount userAccount);
 }
